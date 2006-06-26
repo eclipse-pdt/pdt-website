a8b39b459e4815dde463e668a936e49d6cc248b4
gharpaz <gharpaz>
2006-06-26 13:49:21 +0000

    New Page - build types

---
+++ build_types.php
@@ -0,0 +1,65 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# index.php
+	#
+	# Author: 		Guy Harpaz
+	# Date:			2006-05-01
+	#
+	# Description: downloads
+	#
+	#
+	#****************************************************************************
+	
+	#
+	# Begin: page-specific settings.  Change these. 
+	$pageTitle 		= "PHP IDE Build Types";
+	$pageKeywords	= "PHP, build types";
+	$pageAuthor		= "PHP IDE Committers";
+
+	# Add page-specific Nav bars here
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
+	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
+	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
+
+	# End: page-specific settings
+	#
+		
+	# Paste your HTML content here!
+	ob_start();
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php");
+	$projectInfo = new ProjectInfo("tools.php");
+	
+?>
+<div id="maincontent">
+	<div id="midcolumn">
+
+		<table style="width: 100%;" border="0" cellpadding="2">
+			<tbody>
+				<tr>
+					<td align="left" width="60%"><h1><?=$pageTitle?>
+						<br>
+						<font size="1" color="#8080FF">PHP IDE Installation Instruction</font>
+						</h1>
+					</td> 
+				</tr>
+			</tbody>
+		</table>
+
+		<h2>Under Construction</h2>
+
+	</div>
+
+</div>
+
+
+<?
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
