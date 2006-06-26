e04038d25b847c7e88a3bab300080c152b880ba5
gharpaz <gharpaz>
2006-06-26 13:54:27 +0000

    put content

--- build_types.php
+++ build_types.php
@@ -40,16 +40,67 @@
 		<table style="width: 100%;" border="0" cellpadding="2">
 			<tbody>
 				<tr>
-					<td align="left" width="60%"><h1><?=$pageTitle?>
-						<br>
-						<font size="1" color="#8080FF">PHP IDE Installation Instruction</font>
+					<td align="left" width="60%">
+						<h1>
+							<?=$pageTitle?>
+							<br>
 						</h1>
 					</td> 
 				</tr>
 			</tbody>
 		</table>
 
-		<h2>Under Construction</h2>
+		There are a couple of "dimensions" of the PHP IDE Build Types. 
+
+		<h2>Released Builds</h2>
+		<p> 
+			Releases are builds that have been declared major releases by the committers and PMC- 
+			for example "R1.0". Releases are the right builds for people who want to be on a stable, 
+			tested release, and don't need the latest greatest features and improvements. 
+			Released builds always have an "R" at the beginning of the name i.e. R1.0, R2.0 etc. 
+			Non-release builds are named according to the date of the build - for example 20051027 
+			is the build from Oct 27, 2005.
+		</p>
+		
+		<h2>Maintenance Builds</h2>
+		<p> 
+			Occasionally, we might find there are a few bugs in a Released Build, so a Maintenance 
+			Release will be planned. Until a maintenance release is formally declared and released, 
+			it will be called a "Maintenance Build", and will have a prefix of "M". This roughly 
+			corresponds to "an integration build" but a maintenance build is typically more stable 
+			that an I-Build, since the M-build is based on a declared release (not a new release) 
+			and M-builds contain bug fixes only ... not new function, as I-builds do. 
+			When the maintenance is finalized, approved, and released, it will become a Released Build.
+		</p>
+		
+		<h2>Stable Builds</h2>
+		<p> 
+			Stable builds are integration builds that have been found to be stable enough for most 
+			people to use. They are promoted from integration build to stable build by the architecture 
+			team after they have been used for a few days and deemed reasonably stable. 
+			The latest stable build is the right build for people who want to stay up to date with 
+			what is going on in the latest development stream, and don't mind putting up with a few 
+			problems in order to get the latest greatest features and bug fixes. 
+			The latest stable build is the one the development team likes people to be using, 
+			because of the valuable and timely feedback.
+		</p>
+		
+		<h2>Integration Builds</h2>
+		<p> 
+			All component teams tag their work in what they believe is a stable, consistent state, 
+			and they update the build configuration to indicate that the next integration build should 
+			take this version of the component. Integration builds are built from these versioned 
+			components that have been specified by each component team as the best version available for 
+			the next build.	
+		</p>
+		
+		<h2>Nightly Builds</h2>
+		<p> 
+			Nightly builds are produced every night from whatever has been released into the HEAD 
+			stream of the CVS repository. They are completely untested and will almost always have 
+			major problems. Many will not work at all. These drops are normally only useful to 
+			developers actually working on the Eclipse Project.
+		</p>
 
 	</div>
 
