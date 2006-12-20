34edb22341e16fe0793af8278d5595fbc07ec597
yleon <yleon>
2006-12-20 20:04:36 +0000

    update links

--- _projectCommon.php
+++ _projectCommon.php
@@ -9,10 +9,10 @@
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
-	$Nav->addNavSeparator("Project Home", 	"index.php");
-	$Nav->addCustomNav("Downloads", 		"downloads.php", 	"_self", 2);
-	$Nav->addCustomNav("Installation", 		"install.php", 		"_self", 2);
-	$Nav->addCustomNav("Project Plan", 		"plans/php_plan_0_7.php", 		"_self", 2);
+	$Nav->addNavSeparator("Project Home", 	"http://www.eclipse.org/php/index.php");
+	$Nav->addCustomNav("Downloads", 		"http://www.eclipse.org/php/downloads.php", 	"_self", 2);
+	$Nav->addCustomNav("Installation", 		"http://www.eclipse.org/php/install.php", 		"_self", 2);
+	$Nav->addCustomNav("Project Plan", 		"http://www.eclipse.org/php/plans/php_plan_0_7.php", 		"_self", 2);
 	$Nav->addCustomNav("Wiki", 				"http://wiki.eclipse.org/index.php/PHP", 		"_self", 2);
 	$Nav->addCustomNav("FAQ", 				"faq.php", 			"_self", 2);
 
