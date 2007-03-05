d80c4ff7a4a8e9afc3b6c3524fc4b44041160614
yleon <yleon>
2007-03-05 19:55:17 +0000

    download link

--- _projectCommon.php
+++ _projectCommon.php
@@ -10,7 +10,7 @@
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
 	$Nav->addNavSeparator("Project Home", 	"/pdt/index.php");
-	$Nav->addCustomNav("Downloads", 		"/pdt/downloads.php", 	"_self", 2);
+	$Nav->addCustomNav("Downloads", 		"http://download.eclipse.org/tools/pdt/downloads/", 	"_self", 2);
 	$Nav->addCustomNav("Installation", 		"/pdt/install.php", 		"_self", 2);
 	$Nav->addCustomNav("Project Plan", 		"/pdt/plans/php_plan_0_7.php", 		"_self", 2);
 	$Nav->addCustomNav("Wiki", 				"http://wiki.eclipse.org/index.php/PHP", 		"_self", 2);
