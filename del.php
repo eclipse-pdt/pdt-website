2f1d5303b7d9821762d9bfb653e09c5e14f906d4
slapsha <slapsha>
2007-06-03 14:36:02 +0000

    web site rss feed

---
+++ del.php
@@ -0,0 +1,20 @@
+	              <?
+	              include("rss_reader.php"); 
+	              $reader = new lastRSS();
+	              $messages = $reader->Parse('http://download.eclipse.org/tools/pdt/downloads/rss/center.xml');
+	              $index = 0;
+
+                  while ($index < sizeof($messages["items"]) && $index < 4) {
+	                    $message = $messages["items"][$index];
+	                    ?>
+						<li>
+							<span class="normal"><b><?=$message["title"] ?></b></span> -
+							<a href="<?=html_entity_decode($message["link"]) ?>">Report</a>
+							<a href="javascript:toggle('bugs_<?=$index ?>')">Complete description</a>
+							<ul id="bugs_<?=$index ?>" style="display: none">
+								<li><?=html_entity_decode($message["description"])?></li>
+							</ul>
+						</li>
+	                    <? $index += 1;
+	              }
+	              ?>
