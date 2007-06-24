80af1762317bce5faf498f062bb90ed5507930aa
yleon <yleon>
2007-06-24 18:17:12 +0000

    mylar name change

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ include($App->getProjectCommon());    # All on the same line to unclutter the us
 	
 	$Poll = new Poll(2, "Which enhancement would you like to see implemented in PDT Summer Camp?");
 	$Poll->addOption(1, "Smarty templates support");
-	$Poll->addOption(2, "Mylar integration");
+	$Poll->addOption(2, "Mylyn integration");
 	$Poll->addOption(3, "Smart code assist - recently used elements");
 	$Poll->addOption(4, "Outline view additional filters");
 //	$Poll->addOption(5, "Auto generation of getters and setters");
