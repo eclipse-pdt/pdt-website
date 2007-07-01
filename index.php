b8b9329ff3874e1ee37de577825d80e545ac9c8b
slapsha <slapsha>
2007-07-01 10:36:29 +0000

    Bugzilla Bug 192500 [Website] Errors related to caching
    fixed.

--- index.php
+++ index.php
@@ -283,7 +283,7 @@ include($App->getProjectCommon());    # All on the same line to unclutter the us
 				<h3><a href="http://download.eclipse.org/tools/pdt/downloads/rss/center.xml"><img src="./images/rss.jpg" align="right" title="RSS Feed" alt="[RSS]" /></a>Updates</h3>
 	              <ul>
 	              <?
-				  define('MAGPIE_CACHE_DIR', '/tmp/pdtcache');	              
+		          define('MAGPIE_CACHE_ON', false);
 				  require_once 'rss_fetch.inc';
 				  $url = 'http://download.eclipse.org/tools/pdt/downloads/rss/center.xml';
 				  
