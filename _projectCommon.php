d17830c4f7301471d852bc09b36ef83e89c8e496
slapsha <slapsha>
2007-07-23 18:14:00 +0000

    add ga

--- _projectCommon.php
+++ _projectCommon.php
@@ -15,7 +15,7 @@
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 </script>
 <script type="text/javascript">
-_uacct = "UA-2264768-2";
+_uacct = "UA-2284413-1";
 urchinTracker();
 </script>
 END;
