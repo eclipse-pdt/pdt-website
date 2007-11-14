4320dfe01e1220d84e02b944a527341948d6adfb
yleon <yleon>
2007-11-14 22:53:43 +0000

    who is using the PDT

---
+++ whois.php
@@ -0,0 +1,74 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	#*****************************************************************************
+	#
+	# whois.php
+	#
+	# Author: 		Yossi Leon
+	# Date:			2007-11-14
+	#
+	# Description: Who is using PDT by extending the product or bundling it with other products
+	#
+	#
+	#****************************************************************************
+
+	#
+	# Begin: page-specific settings.  Change these.
+	$pageTitle 		= "Who is Using PDT";
+	$pageKeywords	= "PHP, who, using, Zend, Studio, Zero, Project, APDT";
+	$pageAuthor		= "Yossi Leon";
+
+	ob_start();
+
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php");
+	$projectInfo = new ProjectInfo("tools.php");
+
+?>
+
+<div id="maincontent">
+	<div id="midcolumn">
+
+		<table style="width: 100%;" border="0" cellpadding="2">
+			<tbody>
+				<tr>
+					<td align="left" width="60%"><h1><?=$pageTitle?>
+						<br>
+						<font size="1" color="#8080FF">Which products or projects are using the PDT project</font>
+						</h1>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<h2>Zend Studio for Eclipse</h2> - <a href="http://www.zend.com/products/zend_studio/eclipse/">Product Home Page</a>
+		<p>Zend Studio for Eclipse is Zend Studio’s next generation product which is based 
+		on proven Zend Studio technology and the <b>Eclipse PHP Developers Tools (PDT) project</b>. 
+		Zend Studio for Eclipse creates a unique IDE that provides professional PHP development capabilities 
+		combined with the Eclipse multi-language support and plug-in extension technology that creates the 
+		worlds most powerful PHP IDE. <a href="http://www.zend.com/products/zend_studio/eclipse/">Learn more</a>
+		</p>
+		
+		<h2>IBM REST Development Framework</h2> - <a href="http://www.projectzero.org/wiki/bin/view/">Project Home Page</a>
+		<p>Project Zero introduces a simple environment for creating, assembling and executing 
+		applications based on popular Web technologies. The Project Zero environment includes a 
+		scripting runtime for Groovy and PHP with application programming interfaces optimized for 
+		producing REST-style services, integration mash-ups and rich Web interfaces. Project Zero is 
+		an incubator project started within IBM that is focused on the agile development of the next 
+		generation of dynamic Web applications. <a href="http://www.projectzero.org/wiki/bin/view/">Learn more</a>
+		</p>
+		
+		<h2>APDT - PHP Aspect</h2> - <a href="http://code.google.com/soc/2007/google/appinfo.html?csaid=C71130166CAA177E">Project Home Page</a>
+		<p>phpAspect is a PHP language extension to implement Aspect-Oriented Programming (phpaspect.org).
+		The goal of this project is to write a phpAspect plugin for eclipse in order to make people 
+		able to use phpAspect easily in their projects.<a href="http://code.google.com/soc/2007/google/appinfo.html?csaid=C71130166CAA177E">Learn more</a>
+		</p>
+	</div>
+</div>
+
+
+<?
+	$html = ob_get_contents();
+	ob_end_clean();
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
