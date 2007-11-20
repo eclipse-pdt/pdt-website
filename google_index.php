e94d58df001745977a69d828a6072cb769f88916
rganor <rganor>
2007-11-20 07:51:22 +0000

    add https://www.google.com/analytics reporting

--- google_index.php
+++ google_index.php
@@ -1,6 +1,8 @@
 <?php  		
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
 require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+$App 	= new App();
 ?>
 
 <!-- START: Google Analysis report -->
@@ -12,8 +14,7 @@ urchinTracker();
 <!-- END: Google Analysis report -->
 
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
-$App 	= new App();	
+	
 $Nav	= new Nav();	
 $Menu 	= new Menu();		
 include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
