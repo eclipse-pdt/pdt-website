f873530396738d052722a9681f3c8431c6ad6f9b
apeled <apeled>
2008-09-18 08:57:42 +0000

    *** empty log message ***

--- build/index.php
+++ build/index.php
@@ -1,1 +1,1 @@
-<?php require_once ("_common.php"); require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/build/build-common.php"); ini_set('display_errors', 1); ini_set('error_reporting', E_ALL);?>
+<?php require_once ("_common.php"); require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/build/build-common.php"); ?>
