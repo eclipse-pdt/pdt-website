43f9fc601fcda6211e3dc7b4c566e4563e1fbba2
apeled <apeled>
2008-10-02 13:09:41 +0000

    *** empty log message ***

--- downloads/extras-pdt.php
+++ downloads/extras-pdt.php
@@ -1,7 +1,7 @@
 <?php
 
 $oldrels = array(
-	#"1.0.3" => array("2008/06/03 00:00", "http://download.eclipse.org/tools/pdt/downloads/release.php?release=R20080603"),
+	"1.0.3" => array("2008/06/03 00:00", "http://download.eclipse.org/tools/pdt/downloads/release.php?release=R20080603"),
 );
 
 function showNotes()
@@ -10,10 +10,8 @@ function showNotes()
 	<div class="homeitem3col">
 		<h3>Questions?</h3>
 		<p>If you have problems downloading the drops or with this site, contact the <a href="mailto:webmaster@eclipse.org">webmaster</a>.</p>
-		<!--
-			<p>Looking for the <a href="http://download.eclipse.org/tools/pdt/downloads/index-old.php">old PDT download page</a>? You can find it <a href="http://download.eclipse.org/tools/pdt/downloads/index-old.php">here</a>.</p>
-		-->
-
+		<p>Looking for the <a href="http://download.eclipse.org/tools/pdt/downloads/index-old.php">old PDT download page</a>? You can find it <a href="http://download.eclipse.org/tools/pdt/downloads/index-old.php">here</a>.</p>
+		
 		<p>All downloads are provided under the terms and conditions of the <a href="http://www.eclipse.org/legal/epl/notice.html">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
 	</div>
 <?php
