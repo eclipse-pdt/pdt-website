9328961a0aad7d3ee7f62582e5175691389cf5fc
apeled <apeled>
2008-10-02 11:13:45 +0000

    Modified for new build system - contributed by Nick Boldt

--- build/_common.php
+++ build/_common.php
@@ -37,7 +37,7 @@ $options = array (
 	 * matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
 	 * and /home/www-data/build/requests/dependencies.urls.txt 
 	 * */ 	
-	"regex" => "M200.*/eclipse-SDK-|R-.*200.*/eclipse-SDK-|[MR]200.*/emf-sdo-xsd-SDK-|[MR]200.*/GEF-|wtp-|dltk-|dtp-",
+	"regex" => "M200.*/eclipse-SDK-|R-.*200.*/eclipse-SDK-|[MSR]200.*/emf-sdo-xsd-SDK-|[MSR]200.*/GEF-|wtp-|dltk-|dtp-",
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
