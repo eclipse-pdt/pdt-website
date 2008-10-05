3414da07b18d1afe4b23ed147245a25d809a1e73
apeled <apeled>
2008-10-05 16:04:33 +0000

    [233744] {nickb} replace archived 1.0.3 with links to all in ones

--- downloads/extras-pdt.php
+++ downloads/extras-pdt.php
@@ -1,30 +1,44 @@
 <?php
 
 $oldrels = array(
-	"1.0.3" => array("2008/06/03 00:00", "http://download.eclipse.org/tools/pdt/downloads/release.php?release=R20080603"),
+	#"1.0.3" => array("2008/06/03 00:00", "http://download.eclipse.org/tools/pdt/downloads/release.php?release=R20080603"),
 );
+
+function doRequirements()
+{
+?>
+<div class="homeitem3col">
+	<h3>PDT 1.0.3 All In Ones</h3>
+	<p>These downloads include PDT 1.0.3, Eclipse 3.3, and all other required plugins.</p>
+	<ul>
+		<li style="border:0"><div style="float:right">123M (<a href="/downloads/download.php?file=/tools/pdt/downloads/drops/1.0.3/R200806030000/checksum/pdt-all-in-one-1.0.3-win32.zip.md5">md5</a>)</div>
+			<a href="/downloads/download.php?file=/tools/pdt/downloads/drops/1.0.3/R200806030000/pdt-all-in-one-1.0.3-win32.zip">PDT 1.0.3 <b style="color:green">All-In-One</b> (Windows 32-bit)</a></li>
+		<li style="border:0"><div style="float:right">122M (<a href="/downloads/download.php?file=/tools/pdt/downloads/drops/1.0.3/R200806030000/checksum/pdt-all-in-one-1.0.3-linux-gtk.zip.md5">md5</a>)</div>
+			<a href="/downloads/download.php?file=/tools/pdt/downloads/drops/1.0.3/R200806030000/pdt-all-in-one-1.0.3-linux-gtk.zip">PDT 1.0.3 <b style="color:green">All-In-One</b> (Linux GTK)</a></li>
+		<li><div style="float:right">122M (<a href="/downloads/download.php?file=/tools/pdt/downloads/drops/1.0.3/R200806030000/checksum/pdt-all-in-one-1.0.3-macosx-carbon.zip.md5">md5</a>)</div>
+			<a href="/downloads/download.php?file=/tools/pdt/downloads/drops/1.0.3/R200806030000/pdt-all-in-one-1.0.3-macosx-carbon.zip">PDT 1.0.3 <b style="color:green">All-In-One</b> (Mac OS X Carbon)</a></li>
+	</ul>
+</div>
+<?php
+}
 
 function showNotes()
 {
-
 	global $PWD;
 	$oldNightlies = loadDirSimple($PWD, "N.*", "d");
-	rsort($oldNightlies); 
-	reset($oldNightlies);
- ?>
+?>
 
 	<div class="homeitem3col">
 		<h3>Nightly Builds</h3>
 		<ul>
 		<?php foreach ($oldNightlies as $on)
 		{
-			print "<li><a href=\"http://download.eclipse.org/tools/pdt/downloads/release.php?release=$on\">$on</a></li>\n";
+			print "<li><a href=\"http://download.eclipse.org/tools/pdt/downloads/drops/$on/\">$on</a></li>\n";
 		} ?>
 		</ul>
 	</div>
 
-       
-       <div class="homeitem3col">
+	<div class="homeitem3col">
 		<h3>Questions?</h3>
 		<p>If you have problems downloading the drops or with this site, contact the <a href="mailto:webmaster@eclipse.org">webmaster</a>.</p>
 		<p>Looking for the <a href="http://download.eclipse.org/tools/pdt/downloads/index-old.php">old PDT download page</a>? You can find it <a href="http://download.eclipse.org/tools/pdt/downloads/index-old.php">here</a>.</p>
