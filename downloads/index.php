188cd10522b654b3b10681f31a6aab704ea72791
apeled <apeled>
2008-11-02 15:56:53 +0000

    Added all-in-one

--- downloads/index.php
+++ downloads/index.php
@@ -28,7 +28,7 @@ ob_start();
 /* config */
 
 /* zips that are allowed to be absent from the downloads page (eg., new ones added mid-stream) */
-$extraZips = array("pdt-SDK", "pdt-examples");
+$extraZips = array("pdt-SDK", "pdt-examples", "pdt-all-in-one");
 
 /* $project => sections/Project Name => (prettyname => filename) */
 /* only required if using something other than the default 4; otherwise will be generated */
@@ -39,6 +39,11 @@ $dls = array(
 			"Runtime" => "runtime",
 			"Examples" => "examples",
 			"Automated Tests" => "Automated-Tests",
+		),
+		"All-in-one" => array(
+		 	"Windows" => "win32",
+			"Linux" => "linux",
+			"Mac" => "macosx",
 		),
 	),
 );
