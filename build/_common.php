87b2c48c76d0c80701e28650dfe0f5257c659009
nickb <nickb>
2008-11-16 21:35:06 +0000

    [251304] add entries for 1.0.5 builds

--- build/_common.php
+++ build/_common.php
@@ -28,9 +28,7 @@ $options = array (
 	"BranchAndJDK" => array (
 		"emft.eclipse.org=------------,------------",
 		"2.0.0=HEAD,/opt/sun-java2-5.0",
-
-		"build.eclipse.org=------------,------------",
-		"2.0.0=HEAD,/opt/public/common/ibm-java2-ppc-50",
+		"1.0.5=R1_0_5_20080723,/opt/sun-java2-5.0",
 	),
 
 	/* define a regular expression to be used to collect the most recent 
