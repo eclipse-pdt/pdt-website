29261d3403b4a3a90ec06f159258a6e4d484230c
nickb <nickb>
2008-11-17 03:30:53 +0000

    [233744] fix regex for HEAD builds (using Ganymede deps, not Galileo)

--- build/_common.php
+++ build/_common.php
@@ -35,7 +35,7 @@ $options = array (
 	 * matching dependencies for running a build. See also releng-common/tools/scripts/start_cron.sh
 	 * and /home/www-data/build/requests/dependencies.urls.txt 
 	 * */ 	
-	"regex" => "M200.*/eclipse-SDK-|R-.*200.*/eclipse-SDK-|[MSR]200.*/emf-sdo-xsd-SDK-|[MSR]200.*/GEF-|wtp-|dltk-|dtp-",
+	"regex" => "M200.*/eclipse-SDK-|R-.*200.*/eclipse-SDK-|[MSR]200.*/emf-sdo-xsd-SDK-2.4|[MSR]200.*/GEF-.+-3.4|R3.0/.+/wtp-.+-|R1.0/.+/dltk-|dtp-",
 	
 	"Mapfile_Rule_Default" => 0, // 0: "Use Map, No Tagging=use-false" or 1:"Generate Map, No Tagging=gen-false"
 
