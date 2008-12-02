7ee10107611c2207d3cbf559539f3edc0e981182
nickb <nickb>
2008-12-02 21:01:43 +0000

    [256168] add secondary site for Eclipse 3.3 installation instructions + link to wiki

--- updates/index.php
+++ updates/index.php
@@ -10,7 +10,10 @@ $PRS = array(
 
 function notes()
 {
+	print "<blockquote>For the latest information on how to install PDT, see <a href='http://wiki.eclipse.org/PDT/Installation'>Installing PDT</a>.</blockquote>";
 }
 
-update_manager("PDT", "PHP Development Tools", $PRS, false, true);
+update_manager("PDT", "PHP Development Tools", $PRS, false, true, 
+	array("2.0 Releases (R)" => "2.0/releases/","2.0 Milestones &amp; RCs (S)" => "2.0/milestones/", "2.0 Interim Builds (I &amp; M)" => "2.0/interim/"),
+	array("1.0 Releases (R)" => "releases/", 	"1.0 Milestones &amp; RCs (S)" => "milestones/", 	 "1.0 Interim Builds (I &amp; M)" => "interim/"));
 ?>
