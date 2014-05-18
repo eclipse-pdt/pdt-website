e89d734069f91d786cc608c8db3e63a492ce3af7
Jacek Pospychala <jacek.pospychala@gmail.com>
2014-05-18 09:57:10 +0200

    remove unused "build" dir

--- build/log-viewer.php
+++
@@ -1,2 +0,0 @@
-<?php require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/buildServer-common.php");
-require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/build/log-viewer-common.php"); ?>
--- build/removeRelease.php
+++
@@ -1,2 +0,0 @@
-<?php require_once ($_SERVER['DOCUMENT_ROOT'] . "/modeling/includes/buildServer-common.php");
-require($_SERVER["DOCUMENT_ROOT"] . "/modeling/includes/removeRelease-common.php"); ?>
