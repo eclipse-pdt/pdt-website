fbb1cf4a33c218ae6c3923e662f2ea92e36f485b
Dawid Pakuła <zulus@w3des.net>
2020-05-15 20:28:53 +0200

    Typo in fork button

--- _projectCommon.php
+++ _projectCommon.php
@@ -56,7 +56,7 @@
 
 	// fork me
 	/** @var $App App */
-	$App->setThemeVariables(array('leftnav_html' => '<a href="https://github.com/ecilpse/pdt" class="fork-me"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_orange_ff7600.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>'));
+	$App->setThemeVariables(array('leftnav_html' => '<a href="https://github.com/eclipse/pdt" class="fork-me"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_orange_ff7600.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>'));
 ?>
 
 <style type="text/css">
